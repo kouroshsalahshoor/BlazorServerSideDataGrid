cd3e933c9624f983b54f57071956bb1297ccf893
Kourosh Salahshoor <kourosh23@hotmail.com>
2020-08-16 13:03:14 +0430

    commit 1

---
+++ BlazorServerSideDataGrid/Blazor/Components/ColumnDefinition.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorServerSideDataGrid.Blazor.Components
+{
+    public class ColumnDefinition
+    {
+        public string DataField { get; set; }
+        public string ColumnTitle { get; set; }
+        public string PlaceHolder { get; set; }
+
+        public bool Sortable { get; set; }
+        public SortDirection SortDirection { get; set; }
+
+        public bool Searchable { get; set; }
+        public string SearchText { get; set; }
+    }
+
+    public enum SortDirection
+    {
+        NotSet,
+        Asc,
+        Desc
+    }
+}
